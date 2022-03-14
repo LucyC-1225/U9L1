d6fb744a126808d2c64eb239f8469247d2e464b4
BT_1E10_13 <sample@bths.edu>
2022-03-14 11:41:47 -0400

    Initial commit

---
+++ src/VehicleRunner.java
@@ -0,0 +1,32 @@
+public class VehicleRunner
+{
+    public static void main(String[] args)
+    {
+        Vehicle myVehicle = new Vehicle("Some Vehicle", 6);
+        System.out.println(myVehicle.getName());
+        System.out.println(myVehicle.getWheels());
+        myVehicle.move(10);
+        myVehicle.turn(90);
+        myVehicle.brake(0.25);
+
+        Car myCar = new Car("Honda Civic", 4, "leather");
+        System.out.println(myCar.getName()); // inherited method
+        System.out.println(myCar.getWheels()); // inherited method
+        System.out.println(myCar.getFabric()); // method ONLY available on Car objects
+        myCar.move(30); // inherited method
+        myCar.turn(-90); // inherited method
+        myCar.brake(0.50); // inherited method
+        myCar.honk(); // method ONLY available on Car objects
+        myCar.lockDoors(); // method ONLY available on Car objects
+
+        Bicycle myBike = new Bicycle("Citi Bike", 2, 10);
+        System.out.println(myBike.getName()); // inherited method
+        System.out.println(myBike.getWheels()); // inherited method
+        System.out.println(myBike.getGearCount()); // method ONLY available on Bicycle objects
+        myBike.move(50); // inherited method
+        myBike.turn(45); // inherited method
+        myBike.brake(0.75); // inherited method
+        myBike.ringBell(); // method ONLY available on Bicycle objects
+
+    }
+}
